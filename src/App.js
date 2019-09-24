dc5abed06f67758ce09e4f561c13208c77008457
Denis Lafrance (Ten Gun Design) <v-delafr@microsoft.com>
2019-09-24 15:41:53 -0700

    updated our menu items to navigate with routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return(
+  <div>
+    <h1>HATS PAGE</h1>
+   
+  </div>
+    )
+  }
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact={true} path='/' component={HomePage}/>
+        <Route exact={false} path='/hats' component={HatsPage}/>
+    
+      </Switch>
     </div>
   );
 }
